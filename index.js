f9317674d60be1baba0989b64d710a4f05e15180
AkashPerumalla <19jj1a0436@cej.jntuh.ac.in>
2024-10-24 22:14:10 +0530

    final commit

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
-const express = require("express");
-
-const PORT = 8000;
-
-
-//init app
-const app = express();
-
-//view engine
-app.set("view engine","ejs");
-
-
+const app = require("./app");
+const PORT = process.env.PORT || 8000;
 
 //listen server
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
+
